d0f54f3b34da00b4954b08c14d92fa7015712cbc
Irina <iargina.irina@gmail.com>
2022-12-16 00:27:06 +0200

    update

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,7 +3,7 @@ import css from 'components/FeedbackOptions/FeedBackOptions.module.css';
 
 export class FeedbackOptions extends Component {
   render() {
-    const { options, onLeaveFeedback } = this.props;
+    const { onLeaveFeedback } = this.props;
     return (
       <div className={css.buttonDiv}>
         <button className={css.button} onClick={ev => onLeaveFeedback('good')}>
